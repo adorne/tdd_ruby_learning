7563f999ea04aa7a5d6a7445622fe5101801a011
adorne <doriwield@gmail.com>
2016-03-16 15:59:38 +0500

    Add consonant move.

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,10 +1,27 @@
 def translate str
-  if str[0] == ("a" || "o" || "e" || "i" || "u")
-    str + "ay"
-  elsif (str[0] && str[1] ) == ("a" || "o" || "e" || "i" || "u")
-    str[0] = str.last
-  else
-    str[0] = str.last
-    str + "ay"
+  str = str.split("")
+
+  # найти способ определять в массиве гласные
+
+  if (str.first == ("a" || "e" || "i" || "o" || "u")) || (!str.include?("a" || "e" || "i" || "o" || "u"))
+    str = str.join
+    str = str + "ay"
+  elsif str.first != ("a" || "e" || "i" || "o" || "u") # перемещение согласных в конец строки
+    puts "First is #{str.first}"
+
+    loop do
+      puts "Before push: #{str}"
+      str.push(str.shift)
+      puts "After push: #{str}"
+      puts ""
+      break if str.first == ("a" || "e" || "i" || "o" || "u")
+    end
+
+    str = str.join
+    str = str + "ay"
   end
+
+  puts str
 end
+
+translate("thr")
