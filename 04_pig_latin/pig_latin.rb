14daae0799a6d7fc720492374a15b7eeb9f1b0e5
adorne <doriwield@gmail.com>
2016-03-17 20:26:28 +0500

    All tests are passed.

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,51 +1,44 @@
-def translate s
-  multiword_str = s.split(" ")
+def translate s # метод для анализа строки на кол-во слов
+  multiword_str = s.split(" ") # делаем из строки массив, разделяя её по пробелам
 
   # проверяем кол-во слов
-  if multiword_str.length > 1
-    # puts "#{multiword_str.length} is > 1"
+  if multiword_str.length > 1 # если в массиве больше одного значения
+    s = [] # опустошаем изначальную переменную для новых значений
 
-    s = []
-    multiword_str.each do |e|
-      # puts "Inside each e is #{e}."
+    multiword_str.each do |e| # каждое из слов
+      result = ayize_word e # прогоняем в методе для изменения
 
-      result = ayize_word e
-      s << result
-
-      # puts "In each s is #{s}"
+      s << result # изменённое слово добавляем в массив
     end
 
-    s = s.join(" ")
-
-    puts "After each s is #{s}"
+    s = s.join(" ") # объединяем значения массива в строку через пробелы
   else
-    puts "#{multiword_str.length} is = 1"
-    s = ayize_word s
+    s = ayize_word s # если одно слово, просто меняем его посредством метода
   end
 
-  s
+  s # возвращаем итог
 end
 
-def ayize_word str
-  str_vowels_arr = str.scan(/[aoieu]/)
-  str_arr = str.split("")
+def ayize_word str # метод для анализа и изменения слова
+  str_vowels_arr = str.scan(/[aoieu]/) # делаем массив из всех попадающихся гласных в слове
+  str_arr = str.split("") # делаем ещё один массив из всех букв слова
 
-  if (str_arr.first.match(/[aoieu]/)) || (str_vowels_arr.empty?)
-    str = str + "ay"
-  elsif !str_arr.first.match(/[aoieu]/) # перемещение согласных в конец строки
-    # puts "First is #{str_arr.first}"
+  if (str_arr.first.match(/[aoieu]/)) || (str_vowels_arr.empty?) # если первая буква гласная или гласных нет вообще
+    str = str + "ay" # добавляем кусок
+  elsif !str_arr.first.match(/[aoieu]/) # если первая буква не гласная
+    loop do # каждую согласную перемещаем в конец массива
+      sh = str_arr.shift # вытаскиваем первую букву
+      str_arr.push(sh) # ставим её в конец массива
 
-    loop do
-      # puts "Before push: #{str_arr}"
-      str_arr.push(str_arr.shift)
-      # puts "After push: #{str_arr}"
-      # puts ""
-      break if str_arr.first.match(/[aoieu]/)
+      if sh == "q" && str_arr.first == "u" # случай буквосочетания "qu"
+        str_arr.push(str_arr.shift) # "qu" идёт в конец массива
+      end
+
+      break if (/[aoieu]/).match(str_arr.first) # ломаем петлю, если добрались до гласной
     end
 
-    str = str_arr.join + "ay"
+    str = str_arr.join + "ay" # объединяем массив в строку и добавляем кусок
   end
 
-  puts "Inside ayize_word str is #{str}."
-  str
+  str # возвращаем слово
 end
