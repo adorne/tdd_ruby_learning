44b1f5003882a90bb2b78f35362d696a27c6c0bd
adorne <doriwield@gmail.com>
2016-03-26 17:49:12 +0500

    Finish -_-

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -2,14 +2,24 @@ class Book
   attr_writer :title
 
   def title
+    exc_words = ["and", "in", "the", "of", "a", "an"]
+
     if @title.split(" ").length > 1
       title_arr = @title.split(" ")
       new_title_arr = []
 
       title_arr.each do |word|
-        word = word.capitalize
-        new_title_arr << word
+        if !(exc_words.include? word)
+          word = word.capitalize
+          new_title_arr << word
+        else
+          new_title_arr << word
+        end
       end
+
+      first = new_title_arr.shift
+      first = first.capitalize
+      new_title_arr = new_title_arr.unshift(first)
 
       @title = new_title_arr.join(" ")
     else
