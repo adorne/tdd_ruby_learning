9a9f158595d61f67e04a02979deb5a4b13287541
adorne <doriwield@gmail.com>
2016-03-30 01:41:03 +0500

    Some refactor #1.

---
+++ 09_timer/timer.rb
@@ -0,0 +1,32 @@
+class Timer
+  attr_writer :seconds
+
+  def seconds seconds = 0
+    @seconds = seconds # без этого никак
+  end
+
+  def time_string
+    h = "00"
+    m = "00"
+    s = "00"
+
+    if @seconds > 3600
+      h = (@seconds / 3600)
+      m = ((@seconds % 3600) / 60)
+      s = (((@seconds % 3600) % 60))
+    elsif @seconds > 60
+      m = ((@seconds % 3600) / 60)
+      s = (((@seconds % 3600) % 60))
+    else
+      s = (@seconds)
+    end
+
+    time = "#{pad(h)}:#{pad(m)}:#{pad(s)}"
+
+    return time
+  end
+
+  def pad arg # добавляем нули
+    arg.to_s.rjust(2, "0") # чудесный строковый метод: первый аргумент задаёт кол-во символов в строке (но не ограничивает её), второй - чем её дополнить до этого кол-ва
+  end
+end
